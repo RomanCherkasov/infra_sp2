8c7726a203de42d5a9121358d3fb0ec20af99a06
roman <chiraololo@gmail.com>
2021-10-25 13:13:00 +0700

    rev 1.0

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,7 @@
 FROM python:3.8.5
 
-RUN mkdir /code
+WORKDIR /code
 
-COPY requirements.txt /code
+COPY . .
 
 RUN pip3 install -r /code/requirements.txt
-
-COPY . /code
-WORKDIR /code
-CMD gunicorn api_yamdb.wsgi:application --bind 0.0.0.0:8000 
--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -3,7 +3,6 @@ from datetime import timedelta
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-print(f'---------------------------------{BASE_DIR}----------------------------------------------')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
